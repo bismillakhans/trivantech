dd1a864f9058faa81f732f9a7dd8c5925b0c6f51
bismillakhans <bismillakhans@gmail.com>
2019-04-29 11:37:53 +0530

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.urls import path
 from  . import views
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
 path('', views.index, name='home')
